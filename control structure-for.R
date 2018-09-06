c0174f41ba4af786ec715a60807825dc39c3b192
yijin wu <33663628+yijinwu1@users.noreply.github.com>
2018-09-06 12:47:42 +0800

    Update control structure-for.R

--- control structure-for.R
+++ control structure-for.R
@@ -1,6 +1,22 @@
 #for
-for(i in 1:10) {
-  print(i)
+for(i in 1:5) {
+  print(i) #answer is 1,2,3,4,5
+}
+
+#for next
+for(i in 1:5){
+  if(i==3) {
+    next
+  }
+  print(i) #answer is 1,2,4,5
+}
+
+#for break
+for(i in 1:5){
+  if(i==3) {
+    break
+  }
+  print(i) #answer is 1,2
 }
 
 #rep
